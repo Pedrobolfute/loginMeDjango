9f1bdef6a63117ccd0518bf047673d70d06ee270
-g <pepenike-101@hotmail.com>
2024-12-07 17:08:40 -0300

    new feat: created buttom to cadaster

--- login/templates/static/app_login/js/login_screen.js
+++ login/templates/static/app_login/js/login_screen.js
@@ -1,23 +1,33 @@
 document.querySelector('form').addEventListener('submit', async function(e) {
     e.preventDefault()
-    const formData = new FormData(this)
-    try{
-        const response = await fetch('/login/', {
-            method: 'POST',
-            body: formData,
-        })
-        const data = await response.json()
-        if(data.status === 'success'){
-            window.location.href = '/welcome/'
-        } else if(data.status === 'invalid') {
-            alert(data.message)
-            window.location.href = '/login/'
-        }else if(data.status === 'empty'){
-            alert(data.message)
+
+    const clickButton = e.submitter;
+    const value = clickButton.value
+
+    if (value == 'Enviar'){
+        const formData = new FormData(this)
+        try{
+            const response = await fetch('/login/', {
+                method: 'POST',
+                body: formData,
+            })
+            const data = await response.json()
+            if(data.status === 'success'){
+                window.location.href = '/welcome/'
+            } else if(data.status === 'invalid') {
+                alert(data.message)
+                window.location.href = '/login/'
+            }else if(data.status === 'empty'){
+                alert(data.message)
+            }
+        } catch(error){
+            alert('Error processing solicitation')
         }
-    } catch(error){
-        alert('Error processing solicitation')
+    }else{
+        window.location.href = '/register/'
+        
     }
+
 }
 )
 console.log('login_screen.js')
