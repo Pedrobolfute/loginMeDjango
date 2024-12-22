b30887b7b05cdaa4dac903792c87b1e0fc36d7fa
Pedrobolfute <pepenike-101@hotmail.com>
2024-12-22 01:17:46 -0300

    url redirect

--- login/login/urls.py
+++ login/login/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', include('app_login.urls')),
+    path('', lambda request: redirect('login/', permanent=True)),
     path('register/', include('app_create_account.urls')),
     path('change/', include('app_change_account.urls')), #Mudar para formulario
     path('welcome/', include('app_welcome.urls')),
