4da26550ba3709056d644eb24052756da3ac5057
pedrobolfute <pepenike-101@hotmail.com>
2025-01-26 11:27:16 -0300

    deleted all files to add new file to push it on pythonanywhere

--- login/app_login/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-# from app_create_account.models import User
-
-# Create your models here.
--- login/app_login/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from app_welcome import views as app_welcome_views
-
-urlpatterns = [
-    path('', views.loginMe, name="login"),
-    path('', app_welcome_views.welcome, name="welcome"),
-]
